8a7d97080db55da32dd7915c74945fb6068924b4
souththree <1358270705@qq.com>
2021-09-09 16:45:18 +0800

    add gormtest

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module awesomeProject
 
 go 1.17
+
+require (
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.21.14
+)
---
+++ src/gocontainer/gocontainer_test.go
@@ -0,0 +1,20 @@
+package gocontainer
+
+import (
+	"container/list"  //heap, list, ring
+	"fmt"
+	"testing"
+)
+
+func TestGoContainer(t *testing.T)  {
+	l := list.New()
+	e4 := l.PushBack(4)
+	e1 := l.PushFront(1)
+	l.InsertBefore(3, e4)
+	l.InsertAfter(2, e1)
+
+	// Iterate through list and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+}
