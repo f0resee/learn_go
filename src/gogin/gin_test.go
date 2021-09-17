0eecefbc10d650e84d74aa8a367f0275667341c0
souththree <1358270705@qq.com>
2021-09-17 22:03:06 +0800

    add gin html

--- src/gogin/gin_test.go
+++ src/gogin/gin_test.go
@@ -1,15 +1,23 @@
 package gogin
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"testing"
 )
 
 func TestGinFastExample(t *testing.T)  {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200,gin.H{
-			"message":"pong",
+	r.LoadHTMLFiles("./submitdata.html")
+	r.GET("/submit", func(c *gin.Context) {
+		fmt.Println("test")
+		name := c.Query("name")
+		if name!=""{
+			fmt.Println(name)
+		}
+		c.HTML(http.StatusOK,"submitdata.html",gin.H{
+			"title":name,
 		})
 	})
 	r.Run()
