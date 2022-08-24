2e1df8e100873be395ae697596929de2cd1e0ec7
souththree <1358270705@qq.com>
2022-08-24 22:42:22 +0800

    add tests

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.7.4
+	github.com/pkg/errors v0.9.1
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.14
--- src/gormtest/gormQuickStart_test.go
+++ src/gormtest/gormQuickStart_test.go
@@ -1,6 +1,7 @@
 package gormtest
 
 import (
+	"encoding/json"
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -43,4 +44,40 @@ func TestQuickStart(t *testing.T) {
 	fmt.Println("run here")
 	// Delete - delete product
 	db.Delete(&product, 1)
+}
+
+type Config struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type ConfigItem struct {
+	Name string `gorm:"name"`
+	Item []byte `gorm:"item"`
+}
+
+func TestJsonByte(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("./testbyte.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	cg := Config{
+		Key:   "aaa",
+		Value: "bbb",
+	}
+	data, _ := json.Marshal(cg)
+
+	// Migrate the schema
+	//db.AutoMigrate(&ConfigItem{})
+	c0 := ConfigItem{
+		Name: "config",
+		Item: data,
+	}
+	db.Create(&c0)
+	t.Log(c0)
+	var c ConfigItem
+	db.First(&c)
+	t.Log(c)
+	t.Log(string(c.Item))
 }
